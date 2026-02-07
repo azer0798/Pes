45bc4042c91c8a447f81191e3fb25a7a2e4f57ba
azer0798 <67507076+azer0798@users.noreply.github.com>
2026-02-07 22:36:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -21,15 +21,17 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 app.use(session({
-    secret: 'pes_2026_secret',
+    secret: 'wassitdz_secret_2026',
     resave: false,
     saveUninitialized: true
 }));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', __dirname);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+// قاعدة البيانات المؤقتة
 let accounts = []; 
 
 const ADMIN_USER = "admin";
@@ -37,13 +39,15 @@ const ADMIN_PASS = "pes2026";
 
 // المسارات
 app.get('/', (req, res) => res.render('index', { accounts: accounts }));
+
 app.get('/login', (req, res) => res.render('login'));
+
 app.post('/login', (req, res) => {
     if (req.body.username === ADMIN_USER && req.body.password === ADMIN_PASS) {
         req.session.isAdmin = true;
         res.redirect('/admin-panel');
     } else {
-        res.send("<script>alert('خطأ!'); window.location='/login';</script>");
+        res.send("<script>alert('البيانات خاطئة!'); window.location='/login';</script>");
     }
 });
 
@@ -55,9 +59,9 @@ app.get('/admin-panel', (req, res) => {
 app.post('/add-account', upload.single('imageFile'), (req, res) => {
     if (!req.session.isAdmin) return res.status(403).send("Unauthorized");
     const newAcc = {
-        id: Math.floor(1000 + Math.random() * 9000),
+        id: Math.floor(1000 + Math.random() * 9000), // توليد كود الحساب
         title: req.body.title,
-        price: parseFloat(req.body.price),
+        price: req.body.price,
         players: req.body.players,
         featured: req.body.featured === 'on',
         img: req.file ? '/uploads/' + req.file.filename : 'https://via.placeholder.com/400x200'
@@ -78,4 +82,4 @@ app.get('/logout', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server: http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`WassitDZ Game running on http://localhost:${PORT}`));
